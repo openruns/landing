5c1919e47740efbdc8c201222193483d511d3acf
Almas Sapargali <almassapargali@gmail.com>
2015-11-11 16:57:02 +0600

    some basic landing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,68 @@
 'use strict';
 module.exports = function (grunt) {
   grunt.initConfig({
-        watch: {
-          sass: {
-            files: ['sass/**/*'],
-            tasks: ['sass']
-          },
-          css: {
-            files: ['styles.css'],
-            tasks: ['autoprefixer', 'cssmin']
-          },
-          html: {
-            files: ['index.src.html'],
-            tasks: ['htmlmin']
-          }
+    watch: {
+      sass: {
+        files: ['sass/**/*'],
+        tasks: ['sass']
+      },
+      css: {
+        files: ['styles.css'],
+        tasks: ['autoprefixer', 'cssmin']
+      },
+      html: {
+        files: ['index.src.html'],
+        tasks: ['htmlmin']
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
         },
-        sass: {
-          dist: {
-            options: {
-              style: 'expanded'
-            },
-            files: {
-              'styles.css': 'sass/styles.scss'
-            }
-          }
-        },
-        autoprefixer: {
-          css: {
-              src: 'styles.css',
-              dest: 'styles.prefixed.css'
-          }
-        },
-        cssmin: {
-          combine: {
-            files: {
-              'styles.min.css': ['styles.prefixed.css']
-            }
-          }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    useShortDoctype: true,
-                    removeRedundantAttributes: true,
-                    removeAttributeQuotes: true,
-                    collapseBooleanAttributes: true
-                },
-                files: {
-                    'index.html': 'index.src.html'
-                }
-            },
+        files: {
+          'styles.css': 'sass/styles.scss'
         }
+      }
+    },
+    autoprefixer: {
+      css: {
+        src: 'styles.css',
+        dest: 'styles.prefixed.css'
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'styles.min.css': ['styles.prefixed.css']
+        }
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+          useShortDoctype: true,
+          removeRedundantAttributes: true,
+          removeAttributeQuotes: true,
+          collapseBooleanAttributes: true
+        },
+        files: {
+          'index.html': 'index.src.html'
+        }
+      },
+    },
+    uglify: {
+      minjs: {
+        files: {
+          'scripts.min.js': [
+            'javascripts/jquery-2.1.4.js',
+            'javascripts/bootstrap.js'
+          ]
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -60,5 +70,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask('default', ['autoprefixer', 'cssmin', 'htmlmin', 'watch', 'sass']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['autoprefixer', 'uglify', 'cssmin', 'htmlmin', 'watch', 'sass']);
 };
